ac0780219fd19328d691220b19f572e407ab71f6
Procinogen <22970698+Procinogen@users.noreply.github.com>
2025-08-13 02:41:23 -0400

    added everything

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+import vtbot from 'astro-vtbot';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), vtbot()]
+});
